66c474d32e25df73b58a5595ac76951bc64f3e6e
Nic-Horton <77477203+Nic-Horton@users.noreply.github.com>
2023-01-28 22:36:01 -0600

    Add hover feature to create pixel art

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const container = document.querySelector('#container');
+const singleSquare = document.querySelectorAll('div');
 
 //Makes div's for a grid 16 by 16
 function makeGrid() {
     for (i = 1; i < 17; i++) {
         for(j = 1; j <17; j++) {
             const gridSquare = document.createElement('div');
+            gridSquare.className = 'row'
             container.appendChild(gridSquare);
-            gridSquare.textContent = `${i},${j}`;
+            //gridSquare.textContent = `${i},${j}`;
+                gridSquare.addEventListener('mouseover', () => {
+                    gridSquare.style.backgroundColor = 'black';
+                });
         }
     }
 }
-makeGrid();
+makeGrid(); 
