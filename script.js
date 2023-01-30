7cca5a1fdf3a737803115c1c670f0660ae2c6f66
Nic-Horton <77477203+Nic-Horton@users.noreply.github.com>
2023-01-29 21:25:27 -0600

    Update the grid making function to use css elements

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const container = document.querySelector('#container');
-const singleSquare = document.querySelectorAll('div');
 
 //Makes div's for a grid 16 by 16
-function makeGrid() {
-    for (i = 1; i < 17; i++) {
-        for(j = 1; j <17; j++) {
-            const gridSquare = document.createElement('div');
-            gridSquare.className = 'row'
-            container.appendChild(gridSquare);
-            //gridSquare.textContent = `${i},${j}`;
-                gridSquare.addEventListener('mouseover', () => {
-                    gridSquare.style.backgroundColor = 'black';
-                });
-        }
-    }
-}
-makeGrid(); 
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        container.appendChild(cell).className = "gridCell";
+    };
+};
+
+makeGrid(16,16);
