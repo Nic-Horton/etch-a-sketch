de7f5186873fdad21292e8c722ffd54914daa8dd
Nic-Horton <77477203+Nic-Horton@users.noreply.github.com>
2023-01-30 20:17:33 -0600

    Add slider to change grid size

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 const container = document.querySelector('#container');
 
-//Makes div's for a grid of specified rows and columns
+//Makes div's for a grid of specified rows and columns and clears out when recalled
 function makeGrid(rows, cols) {
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+      };
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
-        cell.innerText = (i + 1);
+        //cell.innerText = (i + 1);
         container.appendChild(cell).className = "gridCell";
     };
+    const mainGrid = document.querySelectorAll('.gridCell')
+    let trigger = false;
+    //Event to change div color to make pixel art
+    mainGrid.forEach((gridCell) => {
+        gridCell.addEventListener('mousemove', () => {
+            if (trigger === true) {
+            gridCell.style.backgroundColor = "black";
+        }
+        });
+        document.addEventListener('mousedown',function() {
+            trigger = true;
+        });
+        document.addEventListener('mouseup', function() {
+            trigger = false;
+        });
+    });
 };
-
 makeGrid(16,16);
 
-const mainGrid = document.querySelectorAll('div')
-let trigger = false;
+let slider = document.getElementById('myRange');
+let output = document.getElementById('output');
+output.innerHTML = slider.value + " x " + slider.value;
 
-mainGrid.forEach((gridCell) => {
-    gridCell.addEventListener('mouseover', () => {
-        if (trigger === true) {
-        gridCell.style.backgroundColor = "black";
-        }
-    });
-    document.addEventListener('mousedown',function() {
-        trigger = true;
-    });
-    document.addEventListener('mouseup', function() {
-        trigger = false;
-    });
-});
+slider.oninput = function() {
+    if (this.value % 8 == 0 ) {
+    output.innerHTML = this.value + " x " + this.value;
+    makeGrid(this.value,this.value);
+    }
+}
+
+
+
+
+
+
